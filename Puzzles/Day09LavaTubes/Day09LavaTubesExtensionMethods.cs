03bb21effdf71319e7854f327207984ef0bed429
Matt Richardson <matt.richardson@octopus.com>
2022-05-01 13:00:30 +1000

    Use .Zip() instead of custom implementation

--- Puzzles/Day09LavaTubes/Day09LavaTubesExtensionMethods.cs
+++ Puzzles/Day09LavaTubes/Day09LavaTubesExtensionMethods.cs
@@ -1,6 +1,3 @@
-using System.Data.Common;
-using System.Diagnostics.CodeAnalysis;
-
 namespace Puzzles.Day09LavaTubes;
 
 public static class Day09LavaTubesExtensionMethods
@@ -17,12 +14,15 @@ public static class Day09LavaTubesExtensionMethods
             yield return func(enumeratedList, item); 
     }
 
-    public static IEnumerable<(int index, T? previous, T current, T? next)> Sandwich<T>(this IEnumerable<T> data)
+    public static IEnumerable<(int index, T? previous, T current, T? next)> Sandwich<T>(this IEnumerable<T> source)
     {
-        return data.ToArray().Sandwich();
+        var data = source.ToArray();
+        var previous = new T?[] { default }.Concat(data);
+        var next = data.Skip(1).Concat(new T?[] { default });
+        return previous.Zip(data, next)
+            .Select((x, index) => (index, x.First, x.Second, x.Third));
     }
     
-    [return: NotNull]
     public static IEnumerable<T> NotNull<T>(this IEnumerable<T?> data)
     {
         return data.Where(x => x != null)!;
@@ -31,19 +31,6 @@ public static class Day09LavaTubesExtensionMethods
     public static int Product<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector)
     {
         return source.Select(selector).Aggregate((accumulator, current) => accumulator * current);
-    }
-    
-    public static IEnumerable<(int index, T? previous, T current, T? next)> Sandwich<T>(this T[] data) 
-    {
-        var lowerBound = data.GetLowerBound(0);
-        var upperBound = data.GetUpperBound(0);
-        for (var index = lowerBound; index <= upperBound; index++)
-        {
-            var previous = index == lowerBound ? default : data[index - 1];
-            var current = data[index];
-            var next = index == upperBound ? default : data[index + 1];
-            yield return (index, previous, current, next);
-        }
     }
 
     public static int[] Parse(this string input)
