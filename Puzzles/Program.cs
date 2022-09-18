acb9fca99da601b846017e1696e37826fc01f43e
Matt Richardson <matt@redgumtech.com.au>
2022-09-18 06:51:12 +0100

    Tidy up

--- Puzzles/Program.cs
+++ Puzzles/Program.cs
@@ -12,23 +12,24 @@ public static class Program
             .GetTypes()
             .Where(x => x.IsAssignableTo(typeof(IPuzzle)))
             .Where(x => !x.IsInterface)
-            .OrderBy(x => x.Name)
-            .Where(x => x.Name.Contains("Day15"));
+            .OrderBy(x => x.Name);
+            //.Where(x => x.Name.Contains("Day15"));
             
         foreach (var puzzle in puzzles)
         {
             Console.WriteLine($"Day {puzzle.Name[3..5]}: {puzzle.Name[5..]}");
             var instance = (IPuzzle)executingAssembly.CreateInstance(puzzle.FullName!)!;
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            Console.WriteLine(" - Part 1: " + instance.CalculatePart1());
-            stopwatch.Stop();
-            Console.WriteLine("           calculation took " + stopwatch.Elapsed);
-            stopwatch.Reset();
-            stopwatch.Start();
-            Console.WriteLine(" - Part 2: " + instance.CalculatePart2());
-            stopwatch.Stop();
-            Console.WriteLine("           calculation took " + stopwatch.Elapsed);
+            Time(1, () => instance.CalculatePart1());
+            Time(2, () => instance.CalculatePart2());
         }
+    }
+
+    private static void Time(int partNumber, Func<object> func)
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        Console.WriteLine(" - part " + partNumber + ": " + func());
+        stopwatch.Stop();
+        Console.WriteLine("           calculation took " + stopwatch.Elapsed);
     }
 }
