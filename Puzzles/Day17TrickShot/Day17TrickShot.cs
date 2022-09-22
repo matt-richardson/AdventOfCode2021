fc462a9df44c77378b4b13d21d2298cf920a1707
Matt Richardson <matt@redgumtech.com.au>
2022-09-22 10:57:51 +0100

    Day 17, part 2

--- Puzzles/Day17TrickShot/Day17TrickShot.cs
+++ Puzzles/Day17TrickShot/Day17TrickShot.cs
@@ -2,17 +2,21 @@ namespace Puzzles.Day17TrickShot;
 
 public class Day17TrickShot : IPuzzle
 {
-    public object CalculatePart1()
+    private readonly Probe[] results;
+
+    public Day17TrickShot()
     {
         var input = Helpers.ReadInputData(nameof(Day17TrickShot))[0];
         var velocityCalculator = new VelocityCalculator(input);
-        return velocityCalculator.Calculate().Max(probe => probe.MaxYPosition);
+        this.results = velocityCalculator.Calculate().ToArray();
+    }
+    public object CalculatePart1()
+    {
+        return results.Max(probe => probe.MaxYPosition);
     }
 
     public object CalculatePart2()
     {
-        throw new NotImplementedException();
-        // var input = Helpers.ReadInputData(nameof(Day17TrickShot))[0];
-        // return CalculatePart2(input);
+        return results.Length;
     }
 }
